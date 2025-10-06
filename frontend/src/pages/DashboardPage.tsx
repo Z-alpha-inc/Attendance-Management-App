7f3572cabfea50488331d7a816aeb8220382e99a
kazuki <begattya425@gmail.com>
2025-10-06 23:09:57 +0900

    画面作成

---
+++ frontend/src/pages/DashboardPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DashboardPage: React.FC = () => {
+  return (
+    <div>
+      <h1>ダッシュボード</h1>
+      {/* ここにダッシュボードの内容が入ります */}
+    </div>
+  );
+};
+
+export default DashboardPage;
