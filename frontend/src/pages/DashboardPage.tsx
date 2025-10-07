1536ec4aaab25fbe29315ee372fa0024be1378e3
kazuki <begattya425@gmail.com>
2025-10-07 21:20:42 +0900

    ログイン画面

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-
 const DashboardPage: React.FC = () => {
   return (
     <div>
       <h1>ダッシュボード</h1>
-      {/* ここにダッシュボードの内容が入ります */}
     </div>
   );
 };
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,13 +1,15 @@
 import React from 'react'; 
+import LoginForm from '../components/LoginForm'; 
 
 function LoginPage() {
   return (
     <div>
       <h1>ログインページ</h1>
-      {/* ここにログインフォームなどが入ります */}
+      <LoginForm />
     </div>
   );
 }
+
 
 // ↓ この行を忘れないようにしてください
 export default LoginPage;
