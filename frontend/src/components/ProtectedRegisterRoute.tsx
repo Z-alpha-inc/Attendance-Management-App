8bc87a6a21c41df5ffef43248e138c6b677fd911
kazuki <begattya425@gmail.com>
2025-10-13 16:19:11 +0900

    .venvを消去

---
+++ frontend/src/components/ProtectedRegisterRoute.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<Props> = ({ children }) => {
+  const token = localStorage.getItem('access_token');
+  const role = localStorage.getItem("role");
+
+  if (token) {
+   if (role == "employee") {
+      return <Navigate to="/dashboard" replace />;
+    }
+
+    if (role == "admin") {
+      return <Navigate to="/dashboard" replace />;
+    }
+  }
+  if (role !== "employee") {
+      return <Navigate to="/login" replace />;
+    }
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
